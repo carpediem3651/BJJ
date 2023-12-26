c750635b6ebe1db58b3f0091c105305f1b7af124
KiHyeok Nam <75459974+carpediem3651@users.noreply.github.com>
2023-12-26 18:06:58 +0900

    2525번 오븐문제

---
+++ src/backjoon/IF/practice/no2525.java
@@ -0,0 +1,21 @@
+package backjoon.IF.practice;
+
+import java.util.Scanner;
+
+public class no2525 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        int C = sc.nextInt();
+
+        // min = 60 * 시 + 분
+        // 요리하는데 걸린 시간 구하기
+
+        // 시 (24시 이상이 될 경우 0시부터 시작하도록한다.)
+        // 분
+
+    }
+}
---
+++ src/backjoon/IF/practice/no2884.java
@@ -0,0 +1,19 @@
+package backjoon.IF.practice;
+
+import java.util.Scanner;
+
+public class no2884 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int hour = sc.nextInt();
+        int min = sc.nextInt();
+
+//        Case1 min이 45분 보다 적다면...
+
+//        Case2 min이 45분 보다 많다면....
+
+            // Case2-1 hour가 0시라면...
+
+    }
+}
